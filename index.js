d594ee6278003fe1d912b980cfa3956d0828d9f9
poojaraju23 <poojaraju23@gmail.com>
2025-06-12 18:12:24 -0400

    Objects(Referene Types)  - Dot Notation and Bracket Notation

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-// Dynamic Typing
+// Reference Types
+// Objects, Arrays, Functions, and more
 
-let name = "Pooja"; 
-let age = 29; 
-let isApproved = true; 
-let firstName = undefined; 
-let lastName = null;  
+// Objects
+// {} object literal
+
+// Object is a collection of key-value pairs
+let person = {
+    name: "Pooja",
+    age : 29
+};
 
 
+
+// Dot Notation - to change the value of a key
+person.name = "Poojitha";
+
+// Also, we use dot notation to read the value of a key
+
+// bracket notation - to change the value of a key
+person["name"] = "Pooji";
+
+console.log(person.name);
+
+// Dot notation is preferred over bracket notation
