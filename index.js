4aacd1f860d2271698bed24e312177d4356407fd
poojaraju23 <poojaraju23@gmail.com>
2025-06-12 18:46:12 -0400

    Types of Functions - Performing a task and calculating a value

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 // Functions are one of the fundamental building blocks in JS 
 // It is basically a set of statements that performs a task or calculates a value
 
-// Boring type
-function greet() {
-     console.log("Hello My World!!!");
-}
+// Types of functions in JS
 
-greet(); // Calling the function // Output: Hello My World!!!
-
-// The above method is simply boring!!
-
-// Functions can have inputs and these inputs can change how the function behaves 
-
-// Interesting type
+// Performing a task
 function greet(name, lastName) // variable as a parameter
 {
     console.log("Hello " + name + " " + lastName);
 
 }
 
-greet("Pooja", "Nagallapati"); // Calling the function with an argument // Output: Hello Pooja Nagallapati
-greet("Poojitha", "Raju") // Calling the function with another argument // Output: Hello Poojitha Raju
+// Calculating a value
+function square(number) // variable as a parameter
+{
+    return number * number; // returns the square of the number
+}
+
+console.log(square(2)); // output: 4
+
