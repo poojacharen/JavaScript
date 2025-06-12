7d0cd534277909f22b9effd8e53b5ae1fe1e0029
poojaraju23 <poojaraju23@gmail.com>
2025-06-12 17:28:48 -0400

    Variables declaration using let and also there are few rules to be followed

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-   // My first JavaScript code
-      console.log("Hello Pooja !!!");
-      
+   // Variable declaration using let
+      let name = "Pooja";
+      console.log(name);
+
+      // few rules for naming the variables.
+      // 1. Cannot be a reseerved keyword
+      // ie., let, if, else, var etc.
+      // 2. Should be meaningful
+      // ie., don not use a, b, c as variable names.
+      // 3. Cannot start with a number
+      // ie ., 1name, 2name etc.
+      // 4. Cannot contain a space or hyphen
+      // ie., let firstName; -- Camel Notation
+      //      let FirstName; -- Pascal Notation
+      //      let firstName = "Pooja"; 
+      //          lastName = "Nagallapati";
