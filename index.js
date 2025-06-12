94d049fbf6c7347412436af5508f4321f91fa3a8
poojaraju23 <poojaraju23@gmail.com>
2025-06-12 17:59:06 -0400

    Dynamic Typing - typeof name = string etc... by using typeof to know which type is the declared variable

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-// In JS we have 2 categories of types
-// 1. Primitive types/ Value types
-// - Number
-// - String
-// - Boolean
-// - Undefined
-// - Null
-// - Symbol
-// 2. Reference types
-// - Objects
-// - Arrays
-// - Functions
+// Dynamic Typing
 
-// Primitive types are immutable, meaning they cannot be changed once created.
+let name = "Pooja"; 
+let age = 29; 
+let isApproved = true; 
+let firstName = undefined; 
+let lastName = null;  
 
-let name = "Pooja"; // String Literal
-let age = 29; // Number Literal
-let isApproved = true; // Boolean Literal
-let firstName = undefined; // Undefined Literal
-let lastName = null; // Null Literal  we use null in situations where we want to explicitly clear the value of a variable
 
-// Reference types are mutable, meaning they can be changed after creation.
