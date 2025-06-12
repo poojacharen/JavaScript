9e236f6fd0fc53188d5cb5115f2ee7819c68e9b0
poojaraju23 <poojaraju23@gmail.com>
2025-06-12 17:39:34 -0400

    Constants

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-   // Variable declaration using let
-      let name = "Pooja";
-      console.log(name);
+ // Constants
 
-      // few rules for naming the variables.
-      // 1. Cannot be a reseerved keyword
-      // ie., let, if, else, var etc.
-      // 2. Should be meaningful
-      // ie., don not use a, b, c as variable names.
-      // 3. Cannot start with a number
-      // ie ., 1name, 2name etc.
-      // 4. Cannot contain a space or hyphen
-      // ie., let firstName; -- Camel Notation
-      //      let FirstName; -- Pascal Notation
-      //      let firstName = "Pooja"; 
-      //          lastName = "Nagallapati";
+//  let interestRate = 0.3;
+//  interestRate = 1;
+//  console.log
+
+ const interestRate = 0.3;
+ interestRate = 1;
+ console.log(interestRate);
+//  interestRate = 1; // This will throw an error because interestRate is a constant
+//  console.log(interestRate); // Output: 0.3
+
+// If we don't need to reassign constant should be your default choice. Otherwise if we need to 
+// reassign a variable, we should use let.
