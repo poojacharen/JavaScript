65a349b54feba01bad37b38f3177a07c150d9359
poojaraju23 <poojaraju23@gmail.com>
2025-06-13 13:03:10 -0400

    Operators Precedence

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-// Operators 
-// In JS there are different kinds of operators. We use this operators along with our variables
-// and constants to create expressions. And with these expressions we can implement logic and algorithms
-// Operators & variables --> Expressions --> Algorithms
+// Operators Precedence 
+// It is the order in which operators are evaluated in an expression. (BODMAS)
 
-// Different kinds of operators
-// Arithmetic Operators, Assignment Operators, Comparison Operators, Logical Operators, Bitwise Operators 
+// eg : 1
+let x = 10;
+let y = 5;
 
-// 6. Bitwise Operators
-// They are used to perform operations on binary numbers
+let z = x + y * 2; // 20
 
-// 00000001 // 1
-// 00000010 // 2
+// eg : 2
+let a = 5;
+let b = 20;
 
-// Bitwise AND (&)
-// The bitwise AND operator (&) compares each bit of two numbers and returns a new number
-// with bits set to 1 only where both numbers had their bits set to 1.
+let c = (a + b) * 3; // 75
 
-let a = 5; // 00000101
-let b = 3; // 00000011
-let c = a & b; // 00000001
-
-console.log(c); // 1
-
-// Bitwise OR (|)
-// This operator compares each bit of two numbers and returns a new number
-// with bits set to 1 where at least one of the numbers had its bit set to 1
-
-let d = 5; // 00000101
-let e = 3; // 00000011
-let f = a | b; // 00000111
-
-console.log(f); // 7
-
-
-
-// Note : refer codewithmosh course for more details on bitwise operators 
 
 
 
