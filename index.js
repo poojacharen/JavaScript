70c826d2f8fb955815f63641239d5ee4f4109bed
poojaraju23 <poojaraju23@gmail.com>
2025-06-16 16:40:01 -0400

    Loops : While

--- index.js
+++ index.js
@@ -2,37 +2,18 @@
 
 // Loops : repeat an action for no. of times
 
-// For Loop
+// While Loop
 
-for (initialization; condition; afterthought) {
+while (condition) {
 
 }
 
-for (let i = 0; i < 5; i++) {
-    console.log("Hello World");
-} // output : 5 hello world
+let i = 0;
 
-
-for (let i = 0; i < 5; i++) {
-     console.log("Hello World", i);
-} // output : Hello world 0 .....Hello world 4
-
-
-
-for (let i = 1; i <= 5; i++) {
-     console.log("Hello World", i);
-} // Output : Hello world 1 ... Hello world 5
-
-
-for (let i = 1; i <= 5; i++) {
-    if (i % 2 !== 0) console.log(i);
-} // output : 1 3 5 ( we get the odd numbers)
-
-for (let i = 5; i >= 1; i--) {
-    if (i % 2 !== 0) console.log(i);
-} // Output : 5 3 1 ( we get odd numbers in reverse order)
-
-
+while (i <= 5) {
+    if(i % 2 !== 0) console.log(i);
+    i++
+} // output : 1 3 5 
 
 
 
