a52533b46b2b506df570dd5ec8c84d40ca716489
poojaraju23 <poojaraju23@gmail.com>
2025-06-17 18:08:50 -0400

    Exercise : Write a function called countTruthy which takes an array and returns a number of truthy elements in this array

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 // Exercise
 
-// Write a function showNumbers that takes a parameter called 'limit'. Should display 'even' if its a even number and 'odd'
-// if its a odd number
-// expected output : 0 "EVEN"
-//                   1 "ODD" ...... 10 "EVEN"
+// Write a function called countTruthy which takes an array and returns a number of truthy elements in this array
 
 
-showNumbers(10);
+const array = ["Pooja", "Charen", "Sai"];
 
-function showNumbers(limit) {
-    for(let i = 0; i <= limit; i++) {
-        if (i % 2 === 0)
-            console.log(i, "EVEN");
-        else
-            console.log(i, "ODD");
+console.log(countTruthy(array));
 
-
-        const message = (i % 2 === 0) ? 'EVEN' : 'ODD'
-        console.log(i, message);
-    
-    }
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+        if (value)
+            count++;
+        return count;
 
 }
-
-
-
