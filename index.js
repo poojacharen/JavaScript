06d63f0504de128b7e38a91bcf098f4de693b81d
poojaraju23 <poojaraju23@gmail.com>
2025-06-17 19:48:47 -0400

    Exercise : Stars // We have a function called showStars with a parameter called rows.

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 // Exercise
 
-// Grade
-// Calculate the grade of a student. There is different marks of a student and we pass this array in a function called
-// calculateGrade. 
+// Stars
+// We have a function called showStars with a parameter called rows. 
 
+showStars(5);
 
-const marks = [80, 80, 50];
+function showStars(rows) {
+    for (let row = 0; row <= rows; row++) {
+        let pattern = '';
+        for (let i = 0; i < row; i++)
+            pattern += '*';
+        console.log(pattern);
 
-// Average = 70
-// 1-59: F
-// 60-69: D
-// 70-79: C
-// 80-89: B
-// 90-100: A
-
-
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks) {
-    const average = calculateAverage(marks);
-
-    if (average >= 0 && average <= 59) 
-        return 'F';
-    if (average >= 60 && average <= 69)
-        return 'D';
-    if (average >= 70 && average <= 79)
-        return 'C';
-     if (average >= 80 && average <= 89)
-        return 'B';
-     if (average >= 90 && average <= 100)
-        return 'A';
-
-
-}
-
-function calculateAverage(array) {
-    let sum = 0;
-    for (let value of array)
-        sum += value;
-    return sum / array.length;
+    }
 }
