217d378fc55eddd2f8917923d25725e0c4ff9ffd
poojaraju23 <poojaraju23@gmail.com>
2025-06-17 19:02:51 -0400

    Exercise : Sum of multiples of 3 and 5 // Create a function called sum, we give it a limit and this function will return the sum of all the multiples of // 3 and 5 from 0 upto to the given limit // Multiples of 3 : 3, 6, 9 // Multiples of 5 : 5, 10

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Exercise
 
-// String Properties
-// Create a function called showProperties. We pass an object and we should diplay all the properties of this object that are
-// of type string
+// Sum of multiples of 3 and 5
+// Create a function called sum, we give it a limit and this function will return the sum of all the multiples of
+// 3 and 5 from 0 upto to the given limit
+
+// Multiples of 3 : 3, 6, 9
+// Multiples of 5 : 5, 10
 
 
-const movie = {
-    title: "Bahubali",
-    releaseYear: 2018,
-    rating: 5.0,
-    director: "S.S.Rajamouli"
-};
+console.log(sum(10));
 
-showProperties(movie);
+function sum(limit) {
+    let sum = 0;
+    for(let i = 0; i <= limit; i++) 
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i;
 
-function showProperties(object) {
-    for (let key in object) {
-        if (typeof object[key] === 'string')
-            console.log(key, object[key]);
-    }
+    return sum;
+       
+    
 }
