62268d332ccd9b40c878b6932649b26180b122ac
poojaraju23 <poojaraju23@gmail.com>
2025-06-17 20:10:27 -0400

    Exercise : Prime Numbers // Create a function called showPrimes, that takes a limit and shows all the prime numbers up to this limit.

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 // Exercise
 
-// Stars
-// We have a function called showStars with a parameter called rows. 
+// Prime Numbers
+// Create a function called showPrimes, that takes a limit and shows all the prime numbers up to this limit. 
 
-showStars(5);
+// Prime (whose factors are only 1 and itself)
+// coposite
 
-function showStars(rows) {
-    for (let row = 0; row <= rows; row++) {
-        let pattern = '';
-        for (let i = 0; i < row; i++)
-            pattern += '*';
-        console.log(pattern);
+// 12 = 1, 2, 3, 4, 6, 12
+// Can be divided evenly by its factors
 
-    }
+// 11 = 1, 11
+// 13 = 1, 13
+
+showPrimes(20);
+
+function showPrimes(limit) {
+    for (let number = 2; number < limit; number++) 
+         
+        
+        if (isPrime(number)) console.log(number);
+    
+    
+    
+}
+
+function isPrime(number) {
+    
+    for (let factor = 2; factor < number; factor++) 
+        if (number % factor === 0) 
+            return false;
+        
+    
+    return true;
 }
