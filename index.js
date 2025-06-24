d24c5df5eb28768b5b85b5d6286ef097b43bd310
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 14:51:21 -0400

    Objects - Enumerating Properties of an Object

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
 // Objects
 
-// Value vs Reference Types :
-// - Value types are stored in the stack and are copied by value
-// - Reference types are stored in the heap and are copied by reference
-// value types : number, string, boolean, null, undefined, symbol
-// refernce types : object, array, function, date, regex
-
-// example 1
-let x = 10;
-let y = x;
-
-x = 20; // both x and y are independent // x = 20, y = 10
+// Enumerating Properties of an Object
+// Object.keys(obj) - returns an array of the object's own enumerable property names
+// Object.values(obj) - returns an array of the object's own enumerable property values
+// Object.entries(obj) - returns an array of the object's own enumerable property [key, value] pairs
+// Object.getOwnPropertNames(obj) - returns an array of all properties (enumerable or not) found directly upon a given object
 
 
-// example 2
-let x = { value: 10 };
-let y = x;
+const circle = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+    }
+};
 
-x.value = 20 // both x and y point to the same object in memory // x = 20, y = 20
+for (let key in circle)
+    console.log(key, circle[key]);
+
+for (let key of Object.keys(circle))
+    console.log(key);
+
+for (let entry of Object.entries(circle))
+    console.log(entry);
+
+if ('color' in circle) console.log('yes');
 
 
-
-// example 3
-let number = 10;
-
-function increase(number) {
-    number++;
-}
-
-increase(number);
-console.log(number); // op - 10 // number is passed by value, so the original value is not changed
-
-
-// example 4
-let obj = { value: 10 };
-
-function increase(obj) {
-    obj.value++;
-}
-
-increase(obj);
-console.log(obj); // op - { value: 11 } // obj is passed by reference, so the original value is changed
