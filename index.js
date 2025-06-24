9a53f262a1658c493ff0fdc8c23a1d0f87a1c087
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 12:51:50 -0400

    Objects - OOP

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-// Exercise
+// Objects
+// Objects are collection of key-value pairs
 
-// Prime Numbers
-// Create a function called showPrimes, that takes a limit and shows all the prime numbers up to this limit. 
+// Example for an Object-oriented Programming(OOP) language
 
-// Prime (whose factors are only 1 and itself)
-// coposite
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw');
+    }
+};
 
-// 12 = 1, 2, 3, 4, 6, 12
-// Can be divided evenly by its factors
-
-// 11 = 1, 11
-// 13 = 1, 13
-
-showPrimes(20);
-
-function showPrimes(limit) {
-    for (let number = 2; number < limit; number++) 
-         
-        
-        if (isPrime(number)) console.log(number);
-    
-    
-    
-}
-
-function isPrime(number) {
-    
-    for (let factor = 2; factor < number; factor++) 
-        if (number % factor === 0) 
-            return false;
-        
-    
-    return true;
-}
+circle.draw(); // method call
