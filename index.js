02db445ee3fc04247a7069f6ba5591cd49b0bcfb
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 15:06:39 -0400

    Objects - Garbage Collection

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 // Objects
 
-// Cloning an Object:
-// It means creating a new object with the same properties as an existing object.
-// This can be done using various methods such as Object.assign(), spread operator or JSON methods
+// Garbage Collection:
+// It is the process of removing unused objects from the heap to free up memory.
+// IN JS, garbage collection is automatic and is handled by the JS engine.
 
 
+let circle = { };
+console.log(circle);
 
-const circle = {
-    radius: 1,
-    draw() {
-        console.log('draw');
-    }
-};
-
-// const another = {};
-// for (let key in circle)
-//     another[key] = circle[key];
-
-const another = Object.assign({}, circle);
-
-const another = { ...circle };
-
-console.log(another);
