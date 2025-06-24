5f6a01b82898269ad6eeafd17a769ffd267098c2
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 15:23:08 -0400

    Objects - String

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Objects
 
-// Math : refer developer.mozilla.org - javascript math object
-// It is a built-in object that has properties and methods for mathematical constants and functions. Not a function object
+// String
+
+// String Primitive
+const message = 'This is my first message';
+
+// String Object
+const another = new String('hi');
 
 
-Math.max(1, 2, 3, 4, 5);
-Math.min(1, 2, 3, 4, 5);
-Math.random(); // returns a random number between 0 and 1
-Math.floor(2.9);  // returns 2 // rounds down to the nearest integer
-
-
-// Note : Refer more for google
+// Note: refer mosh and also google for more
