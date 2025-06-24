433cde5ee5aa5c1fec9eaefe6b43a4323be6c0f8
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 14:38:20 -0400

    Objects - Value vs Reference Types

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 // Objects
 
-// Functions are Objects :
-// Functions are first-class objects in JS. 
-// They can be assigned to variables, passed as arguments and returned from other functions and can have properties
-// Functions are objects, so they can have properties and methods just like any other object in JS
+// Value vs Reference Types :
+// - Value types are stored in the stack and are copied by value
+// - Reference types are stored in the heap and are copied by reference
+// value types : number, string, boolean, null, undefined, symbol
+// refernce types : object, array, function, date, regex
+
+// example 1
+let x = 10;
+let y = x;
+
+x = 20; // both x and y are independent // x = 20, y = 10
 
 
- function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
- }
+// example 2
+let x = { value: 10 };
+let y = x;
 
- Circle.call({}, 1);
- Circle.apply({}, [1,2,3]);
+x.value = 20 // both x and y point to the same object in memory // x = 20, y = 20
 
- const another = new Circle(1);
- 
 
+
+// example 3
+let number = 10;
+
+function increase(number) {
+    number++;
+}
+
+increase(number);
+console.log(number); // op - 10 // number is passed by value, so the original value is not changed
+
+
+// example 4
+let obj = { value: 10 };
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj); // op - { value: 11 } // obj is passed by reference, so the original value is changed
