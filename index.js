c339c0f6bc41a45c44b673fa6b6998ac0c484327
poojaraju23 <poojaraju23@gmail.com>
2025-06-24 13:23:55 -0400

    Objects - Constructor Functions

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // Objects
 
-// Factory Functions
-// A factory function is a function that returns an object. 
-// It is a way to create objects without using the 'new' keyword or constructor functions.
-// Factory functions can be used to create multiple instances of an object with similar properties and methods.
+// Constructor Functions
+// They are used to create objects with a specific structure and behavior.
+// They can be used to create multiple instances of an object with the same properties and methods.
+// Use pascal notation for constructor function names.
 
-// Example of a factory function to create a circle object
-
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('draw');
-        }
-    };
+function Circle(radius) 
+{
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
 }
 
-const circle1 = createCircle(1);
-console.log(circle1);
+const circle = new Circle(1);
 
-const circle2 = createCircle(2);
-console.log(circle2);
+// Difference between Factory Functions and Constructor Functions:
+
+// Factory Functions:
+// They are regular functions that return an object
+// They can return different objects based on the input parameters
+// They can have any name, but it is common to use camelCase notation
+
+// Constructor Functions:
+// They are special functions that are used with the new keyword
+// They always return a new object, even if you don't explicitly return one
+// They should be named using PascalCase notation
