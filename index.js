0c38822d0f688fe4ab344d968e1f1e8e391420e5
poojaraju23 <poojaraju23@gmail.com>
2025-06-25 14:17:17 -0400

    Objects - Exercise 5 - Constructor Function

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-// Exercise - 4 : Blog Post Object
+// Exercise - 5 : Constructor Function
 
-// Create a blog post object with these properties -
-// Title, body, author, views : which represents the number of times this post has been viewed, 
-// comments : each comment should have a couple of properties (author, body)
-// isLive that can be either true or false
-// We should use the object literal syntax to create and initialize a blog post. 
-// GIve each property some value, the actual value doesn't really matter
+// Use a constructor function to create a post object. 
 
 
-// Object Literal Syntax
+// const blog = {
+//     title: 'Pooja',
+//     body: 'Hello this is my blog post',
+//     author: 'Pooja Raju',
+//     views: 5,
+//     comments: [
+//         { author: 'a', body: 'b'},
+//         { author: 'c', body: 'd'},
+//     ],
+//     isLive: true
+// };
 
-const blog = {
-    title: 'Pooja',
-    body: 'Hello this is my blog post',
-    author: 'Pooja Raju',
-    views: 5,
-    comments: [
-        { author: 'a', body: 'b'},
-        { author: 'c', body: 'd'},
-    ],
-    isLive: true
-};
-
+let blog = new Post('Pooja', 'Hi this my blog page', 'Pooja Raju');
 console.log(blog);
 
+
+function Post(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
