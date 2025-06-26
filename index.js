9591e9f29e8631356796f43819e480c6aaef66a9
poojaraju23 <poojaraju23@gmail.com>
2025-06-26 16:34:53 -0400

    Arrays : Exercise 3 : Except

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Exercise 2 : Includes
+// Exercise 3 : Except
 
 const numbers = [1, 2, 3, 4];
 
-console.log(numbers.includes(1));
+const output = except(numbers, [1]);
 
-function includes(array, searchElement) {
-    for (let element of array) {
-        if (element === searchElement)
-            return true;
-    return false;
-    }
+console.log(output);
+
+function except(array, excluded) {
+    const output = [];
+    for (let element of array)
+        if (!excluded.includes(element))
+            output.push(element);
+    return output;
 
 }
-
