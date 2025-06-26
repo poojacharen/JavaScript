542eb6c271f3027e592ed34cdaaf45520b21063b
poojaraju23 <poojaraju23@gmail.com>
2025-06-26 11:50:13 -0400

    Arrays : Arrow Functions

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 // It is a collection of items stored at contiguous memory locations.
 
 
-// Finding Elements (Reference Types): ie; Objects
-// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
-// The includes() method determines whether an array includes a certain elemet, returning true or false as appropriate.
-// The find() method returns the value of the first element in the previous array that satisfies the provided testing function.
-// Otherwise undefined is returned.
-// The findIndex() method returns the index of the first element in the array that satisfies the provided testing function.
+// Arrow Functions :
+// Arrow functions are a more concise syntax for writing function expressions in JS.
+// They are anonymous and change the way this binds in functions.
+// They do not their own context, so they cannot be used as constructors.
+// They are not hoisted, so they cannot be called before they are defined.
 
 
 const courses = [
@@ -16,35 +15,7 @@ const courses = [
     { id: 2, name: 'Azure' },
 ];
 
-// find() method :
-// eg : 1
-const course = courses.find(function(course) {
-    return course.name === 'JavaScript';
-});
+const course = courses.find(course => course.name === 'JavaScript');
 
 console.log(course); // { id: 1, name: 'JavaScript' }
 
-// eg: 2
-const course = courses.find(function(course) {
-    return course.name === 'xyz';
-});
-
-console.log(course); // undefined
-
-
-
-// findIndex method :
-// eg : 1
-
-const courseIndex = courses.findIndex(function(course) {
-    return course.name === 'JavaScript';
-});
-
-console.log(courseIndex); // 0
-
-// eg : 2
-const courseIndex = courses.findIndex(function(course) {
-    return course.name === 'xyz';
-});
-
-console.log(courseIndex); // -1
