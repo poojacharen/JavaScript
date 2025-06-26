ac694b450a6071f44b36b03a6888dbd5812b6d19
poojaraju23 <poojaraju23@gmail.com>
2025-06-26 11:41:39 -0400

    Arrays : Finding Elements (Reference Types : objects)

--- index.js
+++ index.js
@@ -3,29 +3,48 @@
 // It is a collection of items stored at contiguous memory locations.
 
 
-// Finding Elements (Primitive Types):
-// It really depends what we store in an array wheteher it's a primitive or reference type.
-
-const numbers = [1, 2, 3, 1, 4];
-
-// The indexOf() method : returns the first index at which a given element can be found in the array,
-// or -1 if it is not present.
-
-console.log(numbers.indexOf(1)); // 0
-console.log(numbers.indexOf(5)); // -1
-console.log(numbers.indexOf(1, 2)); // 3 // this will start searching from index 2 and return the first occurrence of 1 after that index 
+// Finding Elements (Reference Types): ie; Objects
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+// The includes() method determines whether an array includes a certain elemet, returning true or false as appropriate.
+// The find() method returns the value of the first element in the previous array that satisfies the provided testing function.
+// Otherwise undefined is returned.
+// The findIndex() method returns the index of the first element in the array that satisfies the provided testing function.
 
 
+const courses = [
+    { id: 1, name: 'JavaScript' },
+    { id: 2, name: 'Azure' },
+];
 
-// The lastIndexOf() method : returns the last index at which a given element can be found in the array,
-// or -1 if it is not present.
+// find() method :
+// eg : 1
+const course = courses.find(function(course) {
+    return course.name === 'JavaScript';
+});
 
-console.log(numbers.lastIndexOf(1)); // 3
-console.log(numbers.lastIndexOf(5)); // -1
-console.log(numbers.lastIndexOf(1) !== -1); // true
+console.log(course); // { id: 1, name: 'JavaScript' }
 
-// The includes() method : determines whether an array includes a certain value among its entries,
-// returning true or false as appropriate.
+// eg: 2
+const course = courses.find(function(course) {
+    return course.name === 'xyz';
+});
 
-console.log(numbers.includes(1)); // true
+console.log(course); // undefined
 
+
+
+// findIndex method :
+// eg : 1
+
+const courseIndex = courses.findIndex(function(course) {
+    return course.name === 'JavaScript';
+});
+
+console.log(courseIndex); // 0
+
+// eg : 2
+const courseIndex = courses.findIndex(function(course) {
+    return course.name === 'xyz';
+});
+
+console.log(courseIndex); // -1
