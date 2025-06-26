cb200869db3dd1009166c701d5c0ea4cab8cab15
poojaraju23 <poojaraju23@gmail.com>
2025-06-26 12:46:30 -0400

    Arrays : Combining and Slicing Arrays

--- index.js
+++ index.js
@@ -3,41 +3,36 @@
 // It is a collection of items stored at contiguous memory locations.
 
 
-// Emptying an Array :
-// There are several ways to empty an array in JS
-// 1. Setting the length to 0
-// 2. Reassigning to a new empty array
-// 3. Using splice method
-// 4. Using pop method in a loop
-// 5. Using shift method in a loop
-// 6. Using a filter method to filter out all elements
+// Combining and Slicing Arrays :
+// The concat() method is used to join two or more arrays.
+// It returns a new array without modifying the existing arrays
 
 
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+const first = [1, 2, 3];
+const second = [4, 5, 6];
 
-// Solution 1 : this method is recommended when we have only one reference to the array
-numbers = [];
-console.log(numbers); // []
-console.log(another); // [1, 2, 3, 4]
+const combined = first.concat(second);
+console.log(combined); // [1, 2, 3, 4, 5, 6]
 
-// Solution 2 : This method is also recommended when we have multiple references to the array
+const slice = combined.slice(2, 4);
+console.log(slice); // [3, 4]
 
-numbers.length = 0;
-console.log(numbers); // []
-console.log(another); // [1, 2, 3, 4]
+const slice = combined.slice();
+console.log(slice); // [1, 2, 3, 4, 5, 6]
 
-// Solution 3 
 
-numbers.splice(0, numbers.length);
-console.log(numbers); // []
-console.log(another); // [1, 2, 3, 4]
+// If we hae reference types 
 
-// Solution 4
+const first1 = [{ id: 1 }];
+const second1 = [4, 5, 6];
 
-while (numbers.length > 0)
-    numbers.pop();
+const combined = first.concat(second1);
+first[0].id = 10;
+console.log(combined); // [{ id: 10 }, 4, 5, 6]
 
-console.log(numbers); // []
-console.log(another); // [1, 2, 3, 4]
+const slice = combined.slice();
+console.log(slice); // [{ id: 10 }, 4, 5, 6]
+
+const slice = combined.slice(2, 4);
+console.log(slice); // [5, 6]
 
