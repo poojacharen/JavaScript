c6f04395d9099e9a9f19938886db213d13c5e608
poojaraju23 <poojaraju23@gmail.com>
2025-06-26 13:17:55 -0400

    Arrays : Iterating an Array

--- index.js
+++ index.js
@@ -3,24 +3,19 @@
 // It is a collection of items stored at contiguous memory locations.
 
 
-// The Spread Operator :
-// The spread operator (...) allows an iterable such as an array or string to be expanded in places where zero or more arguments are expected.
-// It is used to unpack elements from an array or properties from an object.
-// It can also be used to combine arrays or objects.
-// The spread operator is useful for copying arrays, merging arrays and passing arrays as function arguments.
+// Iterating an Array
+// The for loop is the most common way to iterate over an array
 
 
-const first = [1, 2, 3];
-const second = [4, 5, 6];
+const numbers = [1, 2, 3];
 
-// const combined = first.concat(second);
-const combined = [...first, ...second]; // Using spread operator to combine arrays
-console.log(combined); // [1, 2, 3, 4, 5, 6]
+for (let number of numbers)
+    console.log(number); // 1 // 2 // 3
 
-const combined = [...first, 'a', ...second, 'b']; // Adding elements while combining
-console.log(combined); // [1, 2, 3, 'a', 4, 5, 6, 'b']
+// The forEach() method calls a function for each element in the array
 
-// const copy = combined.slice();
-const copy = [...combined]; // using spread operator to copy an array
-console.log(copy); // [1, 2, 3, 'a', 4, 5, 6, 'b'] 
+numbers.forEach((number) => console.log(number)); // 1 // 2 // 3
+
+numbers.forEach((number, index) => console.log(index, number)); // 0 1 // 1 2 // 2 3
+
 
