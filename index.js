75e62ea1ae21b489caa0f226f083ea0dfe7ddae2
poojaraju23 <poojaraju23@gmail.com>
2025-07-02 11:59:33 -0400

    Functions : Default Parameters

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 // Functions :
 
-// The Rest Operator
+// Default Parameters 
 
-
-// Eg : 1 
-
-function sum(...args) {
-    return args.reduce((a, b) => a + b);
+function interest(principal, rate = 3.5, years){
+    return principal * rate / 100 * years;
 }
 
-console.log(sum(1, 2, 3, 4, 5, 10)) // op : 25
-
-
-// Eg : 2
-
-function sum(discount, ...prices) {
-    const total = prices.reduce((a,b) => a + b);
-    return total * (1 - discount);
-}
-
-console.log(sum(0.1, 20, 30)); // op : 45
+console.log(interest(10000, undefined, 5)); // op : 1750
 
