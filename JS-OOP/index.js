c5b709dc184cc73ba2a9591598adb1b31887ee2c
poojaraju23 <poojaraju23@gmail.com>
2025-07-15 12:31:04 -0400

    Objects - Constructor Functions

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,15 +1,18 @@
 // Objects
 
-// Factory Function
+// Constructor Functions
+// when we use new keyword to call the function in the constructor function 3 things happen :
+// 1. new operator will create an empty object
+// 2. it will set this to point to that object and 
+// 3. finally it will return that object from the function
 
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
 }
 
-const circle = createCircle(1);
-circle.draw(); // draw
+const another = new Circle(1);
+another.draw(); // draw
