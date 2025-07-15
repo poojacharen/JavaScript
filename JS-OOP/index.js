fd48346c125810eb2162cd3466d41049f074ff07
poojaraju23 <poojaraju23@gmail.com>
2025-07-15 12:36:38 -0400

    Objects - Constructor Property

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,18 +1,9 @@
 // Objects
 
-// Constructor Functions
-// when we use new keyword to call the function in the constructor function 3 things happen :
-// 1. new operator will create an empty object
-// 2. it will set this to point to that object and 
-// 3. finally it will return that object from the function
+// Constructor Property
 
+new String(); // '', "", ``
+new Boolean(); // true, false
+new Number(); // 1, 2, 3, ...
 
-function Circle(radius) {
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
-
-const another = new Circle(1);
-another.draw(); // draw
+// Every object has a constructor property and that references the function that was used to create that object
