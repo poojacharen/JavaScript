face69b3a9000924b77796f1d28216096d521d2c
poojaraju23 <poojaraju23@gmail.com>
2025-07-15 12:20:27 -0400

    Objects - Factory Functions

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,16 +1,15 @@
 // Objects
 
-// Object Literals
+// Factory Function
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
-    }
-};
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
 
+const circle = createCircle(1);
 circle.draw(); // draw
