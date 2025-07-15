08f956b74dd05650a18122059c2deb2765f55692
poojaraju23 <poojaraju23@gmail.com>
2025-07-15 13:41:26 -0400

    Objects - Abstraction

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,27 +1,6 @@
 // Objects
 
-// Enumerating Properties
+// Abstraction : we should hide the details and complexity and show or expose only the essentials
 
-function Circle(radius) {
-    this.radiud = radius;
-    this.draw = function() {
-        console.log('draw');
-    }
-}
 
-const circle = new Circle(10);
-
-// To enumerate all the members in an object we can use "for in" loop 
-for (let key in circle) {
-    if (typeof circle[key] !== 'function')
-        console.log(key, circle[key]);
-}
-
-// to get all the keys use "object.keys"
-const keys = Object.keys(circle);
-console.log(keys);
-
-// check for the existence of a property or a method in an object use the "in" operator
-if('radius' in circle)
-    console.log('Circle has a radius.');
 
