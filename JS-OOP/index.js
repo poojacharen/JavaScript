bde4752795fba7c607bcad0b25c7a233999daf91
poojaraju23 <poojaraju23@gmail.com>
2025-07-16 12:13:08 -0400

    Prototypes - Prototypes and Prototypical Inheritance

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,10 +1,29 @@
 // Prototypes
 
-// Inheritance
+// Prototypes and Prototypical Inheritance
 
-// Classical Inheritance :
-// This is a way to create a new object that inherits properties and methods from another object.
+// Prototypes :
+// A prototype is an object that is asoociated with every function and object in JavaScript.
 
 // eg: 
- // Shape is a parent class
- // Circle is a child class that inherits from Shape
+// Function.prototype is the prototype for all functions
+// Object.prototype is the prototype for all objects
+
+// let x = {};
+// x
+// {}
+// x.__proto__
+// This means that x is an object and its prototype is Object.prototype
+
+// Prototypical Inheritance :
+// It is a feature in JS that allows an object to inherit properties and methods from another object.
+
+// eg:
+// let person = {
+//   name: 'John',
+//   greet: function() {
+//      console.log(`Hello, my name is ${this.name}`);
+//  }
+// }
+
+
