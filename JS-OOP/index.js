59e6fe818cf73fdb8303901d94b0e971c3bd1f50
poojaraju23 <poojaraju23@gmail.com>
2025-07-16 14:44:48 -0400

    Prototypes - Avoid Extending Built-in Objects

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,34 +1,7 @@
 // Prototypes
 
-// Iterating Instance and Prototype Members
-// Instance members are properties and methods defined directly on the instance.
-// Prototype members are properties and methods defined on the prototype of the constructor function.
+// Avoid Extending Built-in Objects
 
-function Circle(radius) {
-    // Instance members
-    this.radius = radius;
-
-    this.move = function() {
-        console.log('move');
-    }
-}
-
-const c1 - new Circle(1);
-
-//  Prototype members
-Circle.prototype.draw = function() {
-    console.log('draw');
-}
-
-c1.draw();
-
-// Returns instance members
-console.log(Object.keys(c1));
-
-// Returns all members (instace + prototype) // instance are also known as own properties
-for (let key in c1) console.log(key);
-
-
-
+//Don't modify built-in objects like array, string etc... as it can lead to unexpected behaviour
 
 
