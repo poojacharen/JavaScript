fbf0500d8f37028be3ee0e548a31a100e5529f19
poojaraju23 <poojaraju23@gmail.com>
2025-07-16 15:16:16 -0400

    Prototypes - Exercise - Stopwatch

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,7 +1,51 @@
 // Prototypes
 
-// Avoid Extending Built-in Objects
+// Exercise 
 
-//Don't modify built-in objects like array, string etc... as it can lead to unexpected behaviour
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    Object.defineProperty(this, 'duration', {
+        get: function() { return duration; },
+        set: function(value) { duration = value; }
+    });
+    Object.defineProperty(this, 'startTime', {
+        get: function() { return startTime; }
+    });
+    Object.defineProperty(this, 'endTime', {
+        get: function() { return endTime; }
+    });
+    Object.defineProperty(this, 'running', {
+        get: function() { return running; }
+    });
+}
 
 
+    Stopwatch.prototype.start = function() {
+        if (this.running)
+            throw new Error('Stopwatch has already started.');
+    this.running = true;
+    this.startTime = new Date();
+    };
+
+    Stopwatch.prototype.stop = function() {
+        if (!this.running)
+            throw new Error('Stopwatch is not started.');
+        this.running = false;
+        this.endTime = new Date();
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        this.duration += seconds;
+    };
+
+    Stopwatch.prototype.reset = function() {
+        this.startTime = null;
+        this.endTime = null;
+        this.running = false;
+        this.duration = 0;
+    };
+
+const sw = new Stopwatch();
+sw.duration = 10;
+
+
+// This is a premature optimization which is the root of all evils.
