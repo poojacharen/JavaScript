9d22c59b92f7e10b0009ac7a71a0a16973a4435f
poojaraju23 <poojaraju23@gmail.com>
2025-07-17 13:24:12 -0400

    Prototypical Inheritance - Resetting The Constructor

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,8 +1,10 @@
 // Prototypical Inheritance
 // This represents a simple example of prototypical inheritance in JS
 
-// Creating your own Prototypical Inheritance
-
+// Resetting The Constructor
+// When you create a new object using a constructor function, the prototype chain is set up.
+// However, if you create a new object using Object.create(), the constructor property is not set to the constructor function.
+// To fix this, you can reset the constructor property of the prototype to point to the constructor function.
 
 function Shape() {
 }
@@ -15,7 +17,11 @@ function Circle() {
     this.radius = this.radius;
 }
 
+// Circle.prototype.constructor = Circle; 
+// new Circle.prototype.constructor() => new Circle()
+
 Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;  // we need to reset the constructor when using the Object.create method
 
 Circle.prototype.draw = function() {
     console.log('draw');
