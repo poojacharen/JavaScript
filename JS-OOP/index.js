4cc8649ee70e3782b4a61e03d7e001309fc13da4
poojaraju23 <poojaraju23@gmail.com>
2025-07-17 16:05:03 -0400

    Prototypical Inheritance - Exercise : Polymorphism

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,7 +1,7 @@
 // Prototypical Inheritance
 // This represents a simple example of prototypical inheritance in JS
 
-// Exercise : Prototypical Inheritance
+// Exercise : Polymorphism
 
 function HtmlElement() {
     this.click = function() {
@@ -23,9 +23,27 @@ function HtmlSelelectedElement(items = []) {
     this.removeItem = function(item) {
         this.items.splice(this.items.indexOf(item), 1);
     }
+
+        this.render = function() {
+    return `
+<select>${this.items.map(item => `
+    <option>${item}</option>`).join('')}
+</select>`;
+ }
 }
 
 HtmlSelelectedElement.prototype = new HtmlElement();
-HtmlSelelectedElement.prototype.constructor = HtmlElement;
+HtmlSelelectedElement.prototype.constructor = HtmlElement
+
+function HtmlImageElement(src) {
+    this.src = src;
+
+    this.render = function() {
+        return `<img src="${this.src}" />`
+    }
+}
+
+HtmlSelelectedElement.prototype = new HtmlElement();
+HtmlSelelectedElement.prototype.constructor = HtmlElement
 
 
