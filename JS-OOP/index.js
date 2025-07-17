587d308d1028fcb07cf232d0a3d2c70958387c54
poojaraju23 <poojaraju23@gmail.com>
2025-07-17 14:01:17 -0400

    Prototypical Inheritance - Method Overriding

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,46 +1,30 @@
 // Prototypical Inheritance
 // This represents a simple example of prototypical inheritance in JS
 
-// Intermediate Function
-// This function will be used to create a prototype chain between Shape and Circle
-// It allows Circle to inherit properties and methods from Shape
-// Shape is the parent class and Circle is the child class
-
-
-function Shape(color) {
-    this.color = color;
-}
-
-Shape.prototype.duplicate = function() {
-    console.log('duplicate');
-}
+// Method Overriding
+// This means that a child class can override methods defined in the parent class
+// In this example, we have a Shape class and two derived classes : Circle and Square
 
 function extend(Child, Parent) {
     Child.prototype = Object.create(Parent.prototype);
     Child.prototype.constructor = Child;
 }
 
-function Circle(radius, color) {
-    Shape.call(this, color);  
-    this.radius = radius;
+function Shape() {
+}
+
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+
+function Circle() {
 }
 
 extend(Circle, Shape);
 
-
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;  
-
-Circle.prototype.draw = function() {
-    console.log('draw');
+Circle.prototype.duplicate = function() {
+    Shape.prototype.duplicate.call(this); // we used this to call the parent method
+    console.log('duplicate circle');
 }
 
-function Square(size) {
-    this.size = size;
-}
-
-extend(Square, Shape);
-
-const s = new Shape();
-const c = new Circle(1, 'blue');
-const sq = new Square(10);
+const c = new Circle();
