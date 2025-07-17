aacde1105450cccfb272474da4508c8580fa3d4b
poojaraju23 <poojaraju23@gmail.com>
2025-07-17 13:47:34 -0400

    Prototypical Inheritance - Intermediate Function

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,9 +1,11 @@
 // Prototypical Inheritance
 // This represents a simple example of prototypical inheritance in JS
 
-// Calling the Super Constructor 
-// In JS, we can create a prototype chain where one object can inherit properties and methods from another object.
-// This is done using the 'Object.create()' method, which creates a new object with the specified prototype object and properties.
+// Intermediate Function
+// This function will be used to create a prototype chain between Shape and Circle
+// It allows Circle to inherit properties and methods from Shape
+// Shape is the parent class and Circle is the child class
+
 
 function Shape(color) {
     this.color = color;
@@ -13,20 +15,32 @@ Shape.prototype.duplicate = function() {
     console.log('duplicate');
 }
 
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
 function Circle(radius, color) {
-    Shape.call(this, color);  // Call the super constructor to initialize the color property
+    Shape.call(this, color);  
     this.radius = radius;
 }
 
-// Circle.prototype.constructor = Circle; 
-// new Circle.prototype.constructor() => new Circle()
+extend(Circle, Shape);
+
 
 Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;  // we need to reset the constructor when using the Object.create method
+Circle.prototype.constructor = Circle;  
 
 Circle.prototype.draw = function() {
     console.log('draw');
 }
 
+function Square(size) {
+    this.size = size;
+}
+
+extend(Square, Shape);
+
 const s = new Shape();
 const c = new Circle(1, 'blue');
+const sq = new Square(10);
