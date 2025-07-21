d4c4f0f60c294fa5ca3ece1947f225e0c7c09b58
poojaraju23 <poojaraju23@gmail.com>
2025-07-21 13:05:25 -0400

    ES6 Classes - Static Methods

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,14 +1,37 @@
 // ES6 Classes :
 
-// Hoisting
-
-// Class decarations are not hoisted
+// Static Methods
+// Static methods are called on the class itself, not an instances of the class.
+// They are often used for utility functions that are related to the class but not require an instance.
 
 class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    // Instance Method // they are defined on the instance of the class and uses 'this' keyword
+    draw() {
+
+    }
+
+    // Static Method // they are defined on the class iteself and uses 'static' keyword
+
+    static parse(str) {
+        const radius = JSON.parse(str).radius;
+        return new Circle(radius);
+    }
 }
 
-// Class Expressions are not hoisted
+const circle = Circle.parse('{ "radius": 1 }');
+console.log(circle);
 
-const Square = class {
-    
-};
+
+// another example of static method
+
+class Math2 {
+    static abs(value) {
+        //...
+    }
+}
+
+Math2.abs() // This is how you call a static method, directly on the class without creating an instance
