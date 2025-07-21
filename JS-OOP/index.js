a0ed55a96f9b746bee265a85fca3efc82b47649d
poojaraju23 <poojaraju23@gmail.com>
2025-07-21 14:26:25 -0400

    ES6 Classes - Exercise : Stack

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,20 +1,38 @@
 // ES6 Classes :
 
-// Method Overriding
+// Exercise : Stack
 
-class Shape {
-    move() {
-        console.log('move')
+const _items = new WeakMap();
+
+class Stack {
+    constructor() {
+        _items.set(this []);
+    }
+
+    push(obj) {
+        _items.get(this).push(obj);
+    }
+
+    pop() {
+        const items = _items.get(this);
+
+        if(items.length === 0)
+            throw new Error('Stack is empty.');
+
+        return items.pop();
+    }
+
+    peek() {
+        const items = _items.get(this);
+
+        if(items.length === 0)
+            throw new Error('Stack is empty.');
+
+        return items[items.length - 1];
+    }
+
+    get count() {
+        return _items.get(this).length;
     }
 }
-
-class Circle extends Shape {
-    move() {
-        super.move();
-        console.log('circle move');
-    }
-}
-
-const c = new Circle();
-
    
