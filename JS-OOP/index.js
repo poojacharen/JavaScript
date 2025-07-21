bd680e7773faab4cd3333f97f085612f8c065490
poojaraju23 <poojaraju23@gmail.com>
2025-07-21 13:49:36 -0400

    ES6 Classes - Private Properties/Members Using WeakMaps

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,27 +1,24 @@
 // ES6 Classes :
 
-// Private Members Using Symbols 
-// This is a way to create private members in ES6 classes using symbols
-// This is not a true private member, but it is a way to create a member that is not easily accessible
+// Private Properties/Members Using WeakMaps
 
-const _radius = Symbol();
-const _draw = Symbol();
+const _radius = new WeakMap();;
+const _move = new WeakMap();
 
 class Circle {
     constructor(radius) {
-        this[_radius] = radius;
+        _radius.set(this, radius);
+        _move.set(this, () => {
+            console.log('move', this);
+        });
     }
 
-    [_draw] () {
+    draw() {
+        _move.get(this)();
 
+        console.log('draw');
     }
 }
 
 const c = new Circle(1);
-const key = Object.getOwnPropertySymbols(c)[0];
-console.log(c[key]);
-
-
-
-
    
