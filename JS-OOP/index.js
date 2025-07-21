6a4c3602558ff8371a6dec0dd36b48d83ad76aa5
poojaraju23 <poojaraju23@gmail.com>
2025-07-21 13:22:03 -0400

    ES6 Classes - The "this" Keyword

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,37 +1,37 @@
 // ES6 Classes :
 
-// Static Methods
-// Static methods are called on the class itself, not an instances of the class.
-// They are often used for utility functions that are related to the class but not require an instance.
+// The "this" Keyword
+// The "this" keyword refers to the object that is executing the currennt function
+
+'use strict';
+
+const Circle = function() {
+    this.draw = function() {
+        console.log(this);
+    }
+};
+
+const c = new Circle();
+
+// Method Call // "this" refers to the object that is executing the current function
+c.draw();
+
+const draw = c.draw;
+
+// Function Call
+draw(); // undefined in strict mode, global object in non-strict mode
+
+
+
+// Another example
 
 class Circle {
-    constructor(radius) {
-        this.radius = radius;
-    }
-
-    // Instance Method // they are defined on the instance of the class and uses 'this' keyword
     draw() {
-
-    }
-
-    // Static Method // they are defined on the class iteself and uses 'static' keyword
-
-    static parse(str) {
-        const radius = JSON.parse(str).radius;
-        return new Circle(radius);
+        console.log(this);
     }
 }
 
-const circle = Circle.parse('{ "radius": 1 }');
-console.log(circle);
-
-
-// another example of static method
-
-class Math2 {
-    static abs(value) {
-        //...
-    }
-}
-
-Math2.abs() // This is how you call a static method, directly on the class without creating an instance
+const c = new Circle();
+const draw = c.draw;
+draw(); //  op - undefined
+   
