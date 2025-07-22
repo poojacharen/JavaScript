3e4add84b07e551b34dd11a0895b0a4b892dc332
poojaraju23 <poojaraju23@gmail.com>
2025-07-22 13:33:57 -0400

    ES6 Tooling - ES6 Tools

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,8 +1,9 @@
 // ES6 Tooling
 
-// ES6 Modules
+// ES6 Tooling
 
-import {Circle} from './circle.js';
-
-const c = new Circle(10);
-c.draw();
+// Two kinds of tools
+// 1. Transpiler : Is a combination of two words. It is a translator and a compiler. 
+//                 It translates the code from one language to another language and compiles it.
+// 2. Bundler : It is a tool that takes multiple files and combines them into a single file. 
+//              The tool that combines to one single file is Webpack
