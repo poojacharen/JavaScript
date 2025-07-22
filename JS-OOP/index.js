135ed85ef5d363b6274d0caac82694cf297d06dd
poojaraju23 <poojaraju23@gmail.com>
2025-07-22 13:14:42 -0400

    ES6 Tooling - CommonJS Modules

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,9 +1,8 @@
 // ES6 Tooling
 
-// Modules
-// There are many ways to define modules in JS
-// But the most common modules are CommonJS and ES6 modules
-// CommonJS is used in node.js
-// ES6 modules are used in modern browsers and can be transpiled to CommonJS
+// CommonJS Modules
+// It is used to import modules in Node.js and other environments that support CommonJS
+
+
 
 
