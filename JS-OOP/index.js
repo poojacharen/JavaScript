46ff2d3bea8923ec322a15409cc93dd9311bd788
poojaraju23 <poojaraju23@gmail.com>
2025-07-22 14:14:24 -0400

    ES6 Tooling - Babel

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,9 +1,4 @@
 // ES6 Tooling
 
-// ES6 Tooling
+// Babel
 
-// Two kinds of tools
-// 1. Transpiler : Is a combination of two words. It is a translator and a compiler. 
-//                 It translates the code from one language to another language and compiles it.
-// 2. Bundler : It is a tool that takes multiple files and combines them into a single file. 
-//              The tool that combines to one single file is Webpack
